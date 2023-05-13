4f3af64a1437abcc5086422e29b08655eb0c98c4
f-davin <8143811+DavTechNet@users.noreply.github.com>
2023-05-13 18:30:53 +0200

    feat: update unit tests about decode method

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,21 +1,30 @@
+import pytest
+
 from smartclim_ble.parser import SensorData
 
 
 def test_sensor_data_decode():
     # Advertising frame
     sensor = SensorData()
-    data_bytes = bytearray.fromhex("0500de00023b070000062e")
-    sensor.decode(data_bytes, True)
+    sensor.decode(bytearray.fromhex("0500de00023b070000062e"), True)
     assert sensor.temperature == 22.2
     assert sensor.humidity == 59
     assert sensor.battery == 46
 
     # Connected frame
-    data_bytes = bytearray.fromhex("00de00023b070000062e")
-    sensor.decode(data_bytes, False)
+    sensor.decode(bytearray.fromhex("00de00023b070000062e"), False)
     assert sensor.temperature == 22.2
     assert sensor.humidity == 59
     assert sensor.battery == 46
 
-    # Wrong frame
-    assert True
+    # Negatives temperatures
+    sensor.decode(bytearray.fromhex("05003A000227070000062C"), True)
+    assert sensor.temperature == 5.8
+    sensor.decode(bytearray.fromhex("0500FDFF0228070000062C"), True)
+    assert sensor.temperature == -0.2
+    sensor.decode(bytearray.fromhex("0500EAFF0229070000062C"), True)
+    assert sensor.temperature == -2.1
+
+    # Wrong frame size
+    with pytest.raises(Exception):
+        sensor.decode(bytearray.fromhex("0500de00023b070000062e22"))

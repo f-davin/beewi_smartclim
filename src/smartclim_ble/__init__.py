b719e1a2885fc901f42e6b1d085503e4525e067a
f-davin <8143811+f-davin@users.noreply.github.com>
2023-07-28 17:23:23 +0200

    feat: add new class about the beewi smart clim

--- src/smartclim_ble/__init__.py
+++ src/smartclim_ble/__init__.py
@@ -1,5 +1,33 @@
 """Module initialisation."""
 
-from .parser import BeeWiSmartClimAdvertisement, SmartClimSensorData
+from sensor_state_data import (
+    DeviceClass,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .parser import (
+    BeeWiSmartClimAdvertisement,
+    SmartClimSensorData,
+    XiaomiBluetoothDeviceData,
+)
 
 __version__ = "0.3.0"
+
+__all__ = [
+    "BeeWiSmartClimAdvertisement",
+    "SmartClimSensorData",
+    "XiaomiBluetoothDeviceData",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceClass",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+]
